42f47a8f357681e6aba1239beeeb0c76f3ff7559
Wellinton Piassa <wellintonpiassa@hotmail.com>
2024-02-21 14:00:27 -0300

    fix: fixing arrow to work with router push from next navigation

--- app/components/arrow/Arrow.tsx
+++ app/components/arrow/Arrow.tsx
@@ -1,14 +1,16 @@
 import './arrow.css'
 import { Fade } from "react-awesome-reveal"
 import Image from 'next/image';
+import { useRouter } from 'next/navigation';
 
 export default function Arrow({direction="down", delay=1000, link=""}:any) {
+  const router = useRouter();
   return (
     <Fade 
       delay={delay}
       triggerOnce={true}
       childClassName={`arrow ${direction}`} >
-        <a href={link}>
+        <a href='#' onClick={() => router.push(link)}>
           <Image
             src="/icons/arrow.svg"
             width={30}
