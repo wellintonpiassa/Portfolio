c06301eed18f3e4443fe09ded09827b76d94f591
Wellinton Piassa <wellintonpiassa@hotmail.com>
2024-02-21 13:26:23 -0300

    fix: fixing automatic theme switching

--- app/components/header/Header.tsx
+++ app/components/header/Header.tsx
@@ -1,25 +1,32 @@
 'use client'
-import { useState } from 'react';
 import './header.css';
+import { useEffect, useState } from 'react';
+import { useParams, useRouter } from 'next/navigation';
 
 export default function Header() {
 
-  const [theme, setTheme] = useState(detectTheme());
+  const [theme, setTheme] = useState('')
+  const router = useRouter();
+  const params = useParams();
 
-  function detectTheme() {
+  function pushToPage (link:string) {
+    router.push(link);
+  }
+
+  useEffect(() => {
     const darkPages = ['#contact'];
     var currHash = window.document.location.hash;
-    return (darkPages.includes(currHash)) ? 'dark': 'light';
-  }
+    (darkPages.includes(currHash)) ? setTheme('dark') : setTheme('light')
+  });
 
   return (
     <div id="header" className={`container-fluid d-flex justify-content-end align-items-center fixed-top ${theme}`}>
-      <ul className="d-flex gap-5 pe-5">'
-        <li><a className='menu-link' onClick={ () => setTheme('light') } href="#intro">Home</a></li>
-        <li><a className='menu-link' onClick={ () => setTheme('light') } href="#about">Sobre</a></li>
-        <li><a className='menu-link' onClick={ () => setTheme('light') } href="#skills">Habilidades</a></li>
-        <li><a className='menu-link' onClick={ () => setTheme('light') } href="#projects">Projetos</a></li>
-        <li><a className='menu-link' onClick={ () => setTheme('dark') } href="#contact">Contato</a></li>
+      <ul className="d-flex gap-5 pe-5">
+        <li><a className='menu-link' onClick={() => pushToPage('#intro')}>Home</a></li>
+        <li><a className='menu-link' onClick={() => pushToPage('#about')}>Sobre</a></li>
+        <li><a className='menu-link' onClick={() => pushToPage('#skills')}>Habilidades</a></li>
+        <li><a className='menu-link' onClick={() => pushToPage('#projects')}>Projetos</a></li>
+        <li><a className='menu-link' onClick={() => pushToPage('#contact')}>Contato</a></li>
       </ul>
     </div>
   )
