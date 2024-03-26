744aab973ffc4a27f7791bd4b759280dace3c7f8
Wellinton Piassa <wellintonpiassa@hotmail.com>
2024-03-25 23:32:59 -0300

    feat: new animation on skills cards

--- app/pages/skills/Skills.tsx
+++ app/pages/skills/Skills.tsx
@@ -41,85 +41,87 @@ export default function Skills(props: any) {
             delay={500}>
               <div className='card'>
                 <ul>
-                  <li>
-                    <Html5Original size={80}/>
-                    <span>HTML</span>
-                  </li>
-                  <li>
-                    <Css3Original size={80}/>
-                    <span>CSS</span>
-                  </li>
-                  <li>
-                    <BootstrapOriginal size={80}/>
-                    <span>Bootstrap</span>
-                  </li>
-                  <li>
-                    <JqueryOriginal size={80}/>
-                    <span>jQuery</span>
-                  </li>
-                  <li>
-                    <NextjsOriginal size={80}/>
-                    <span>Next JS</span>
-                  </li>
-                  <li>
-                    <COriginal size={80}/>
-                    <span>C</span>
-                  </li>
-                  <li>
-                    <JavaOriginal size={80}/>
-                    <span>Java</span>
-                  </li>
-                  <li>
-                    <JavascriptOriginal size={80}/>
-                    <span>Javascript</span>
-                  </li>
-                  <li>
-                    <TypescriptOriginal size={80}/>
-                    <span>TypeScript</span>
-                  </li>
-                  <li>
-                    <PythonOriginal size={80}/>
-                    <span>Python</span>
-                  </li>
-                  <li>
-                    <PhpOriginal size={80}/>
-                    <span>PHP</span>
-                  </li>
-                  <li>
-                    <MysqlOriginal size={80}/>
-                    <span>MySQL</span>
-                  </li>
-                  <li>
-                    <PostgresqlOriginal size={80}/>
-                    <span>PostgreSQL</span>
-                  </li>
-                  <li>
-                    <BashOriginal size={80}/>
-                    <span>Bash</span>
-                  </li>
-                  <li>
-                    <GitOriginalWordmark size={80}/>
-                    <span>Git</span>
-                  </li>
-                  <li>
-                    <LinuxOriginal size={80}/>
-                    <span>Linux</span>
-                  </li>
-                  <li className='disabled'>
-                    <DockerOriginal size={80} />
-                    <span>Docker</span>
-                    <span>Aprendizado em andamento</span>
-                  </li>
-                  <li className='disabled'>
-                    <LaravelOriginal size={80}/>
-                    <span>Laravel</span>
-                    <span>Aprendizado em andamento</span>
-                  </li>
-                  <li className='disabled'>
-                    <ReactOriginal size={80}/>
-                    <span>React Native</span>
-                    <span>Aprendizado em andamento</span>
-                  </li>
+                  <Fade cascade damping={0.1} direction='up' >
+                    <li>
+                      <Html5Original size={80}/>
+                      <span>HTML</span>
+                    </li>
+                    <li>
+                      <Css3Original size={80}/>
+                      <span>CSS</span>
+                    </li>
+                    <li>
+                      <BootstrapOriginal size={80}/>
+                      <span>Bootstrap</span>
+                    </li>
+                    <li>
+                      <JqueryOriginal size={80}/>
+                      <span>jQuery</span>
+                    </li>
+                    <li>
+                      <NextjsOriginal size={80}/>
+                      <span>Next JS</span>
+                    </li>
+                    <li>
+                      <COriginal size={80}/>
+                      <span>C</span>
+                    </li>
+                    <li>
+                      <JavaOriginal size={80}/>
+                      <span>Java</span>
+                    </li>
+                    <li>
+                      <JavascriptOriginal size={80}/>
+                      <span>Javascript</span>
+                    </li>
+                    <li>
+                      <TypescriptOriginal size={80}/>
+                      <span>TypeScript</span>
+                    </li>
+                    <li>
+                      <PythonOriginal size={80}/>
+                      <span>Python</span>
+                    </li>
+                    <li>
+                      <PhpOriginal size={80}/>
+                      <span>PHP</span>
+                    </li>
+                    <li>
+                      <MysqlOriginal size={80}/>
+                      <span>MySQL</span>
+                    </li>
+                    <li>
+                      <PostgresqlOriginal size={80}/>
+                      <span>PostgreSQL</span>
+                    </li>
+                    <li>
+                      <BashOriginal size={80}/>
+                      <span>Bash</span>
+                    </li>
+                    <li>
+                      <GitOriginalWordmark size={80}/>
+                      <span>Git</span>
+                    </li>
+                    <li>
+                      <LinuxOriginal size={80}/>
+                      <span>Linux</span>
+                    </li>
+                    <li className='disabled'>
+                      <DockerOriginal size={80} />
+                      <span>Docker</span>
+                      <span>Aprendizado em andamento</span>
+                    </li>
+                    <li className='disabled'>
+                      <LaravelOriginal size={80}/>
+                      <span>Laravel</span>
+                      <span>Aprendizado em andamento</span>
+                    </li>
+                    <li className='disabled'>
+                      <ReactOriginal size={80}/>
+                      <span>React Native</span>
+                      <span>Aprendizado em andamento</span>
+                    </li>
+                  </Fade>
                 </ul>
               </div>
             </Fade>
